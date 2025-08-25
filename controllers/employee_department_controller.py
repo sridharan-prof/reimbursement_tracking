eb18957ff90ae3c2cc4b70ed19eaefe9dcf6076f
sridharan-prof <sridharan.prof@gmail.com>
2025-08-25 21:23:20 +0530

    modified

--- controllers/employee_department_controller.py
+++ controllers/employee_department_controller.py
@@ -6,6 +6,6 @@ class EmployeeDepartmentsFrontend(http.Controller):
     @http.route('/departments', auth='public', website=True)
     def departments_page(self, **kwargs):
         departments = request.env['employee.departments'].sudo().search([])
-        return request.render('reimbursement_details.departments_page_template', {
+        return request.render('reimbursement_tracking.departments_page_template', {
             'departments': departments
         })
